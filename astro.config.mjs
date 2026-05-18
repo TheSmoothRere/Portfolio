4fdd43f4eaa2669162bf7f9dab4ca8ffc80be288
TheSmoothRere <thesmoothrere@gmail.com>
2026-05-18 22:09:22 +0700

    feat: scaffold project structure with Tailwind CSS, layout components, and essential Astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), robotsTxt()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
